2df9989142b4c8796eaeaa0f45087a330491d9d7
MaximeA <44642448+maxime-a@users.noreply.github.com>
2021-11-26 22:30:13 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,35 @@ function activateCountdown(element, dateString){
     },250);
 }
 
+function activateCountdownWeeks(element, dateString){
+    const targetDate = dayjs(dateString);
+    element.querySelector(".until__event").textContent = `Until ${ targetDate.format("D MMMM YYYY")}`;
+
+    setInterval(()=>{
+        const now = dayjs();
+        const duration = dayjs.duration(targetDate.diff(now));
+
+        element.querySelector(".until__numeric--weeks").textContent = duration.weeks().toString().padStart(2,"0");    
+    },250);
+}
+
+function activateCountdownWeekends(element, dateString){
+    const targetDate = dayjs(dateString);
+    element.querySelector(".until__event").textContent = `Until ${ targetDate.format("D MMMM YYYY")}`;
+
+    setInterval(()=>{
+        const now = dayjs();
+        const duration = dayjs.duration(targetDate.diff(now));
+
+        numberOfWeekends = Math.floor(duration.asDays()/7);
+        if((now.day()+1)%7+duration.asDays()%7 >= 8){numberOfWeekends+=1};
+
+        element.querySelector(".until__numeric--weekends").textContent = numberOfWeekends.toFixed(0).toString().padStart(2,"0");    
+        console.log((now.day()+1)%7+duration.asDays()%7);
+        console.log(numberOfWeekends);
+    },250);
+}
+
 activateCountdown(document.getElementById("MyCountdown"), "2021-12-18");
+activateCountdownWeeks(document.getElementById("MyCountdown2"), "2021-12-18");
+activateCountdownWeekends(document.getElementById("MyCountdown3"), "2021-12-18");
